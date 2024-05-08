a29741544fe576270c07de35242ff179d8028280
bgni1509 <bgni1509@gmail.com>
2024-05-08 10:53:56 +0300

    change_style

---
+++ js/scripts.js
@@ -0,0 +1,13 @@
+$('.owl-carousel').owlCarousel({
+    autoplay:true,
+    autoplayTimeout:1000,
+    loop: true,
+    responsive:{
+        0:{
+            items:1
+        },
+        1100:{
+            items:4
+        }
+    }
+});
