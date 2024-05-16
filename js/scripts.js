f3672c0ed91bbb9d5b330d11330a6d03f6206239
bgni1509 <bgni1509@gmail.com>
2024-05-16 13:49:32 +0300

    change_style

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,28 @@
 $('.owl-carousel').owlCarousel({
-    autoplay:true,
-    autoplayTimeout:1000,
+    // autoplay:true,
+    // autoplayTimeout:1000,
     loop: true,
     nav: true,
     responsive:{
         0:{
+            items:1
+        },
+        1099:{
+            items:4
+        },
+        992:{
+            items:3
+        },
+        768:{
+            items:3
+        },
+        540:{
+            items:2
+        },
+        320:{
+            items:1
+        },
+        280:{
             items:1
         },
         1100:{
